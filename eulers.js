3f8945445f0a96d8bc379bf8ac916786f177ea0f
jonnynewbs <jonnynewbs@gmail.com>
2013-04-01 22:53:54 +0800

    Improve logging

--- eulers.js
+++ eulers.js
@@ -7,7 +7,7 @@ function get_path(user, prob) {
 
 function run_attempt(prob, user, ans, path, start_time) { //capture problem number, user, answer, path and start time
   fs.readFile(path, 'utf8', function (err, data) {
-    console.log("===\nUser: " + user + ", problem: " + prob + ", Start time: " + start_time + ".")
+    console.log("===\nProblem: " + prob + ", User: " + user + ", Start time: " + start_time + ".")
     if (err) {
       console.log("Unable to read solution script.");
       return;
@@ -16,13 +16,12 @@ function run_attempt(prob, user, ans, path, start_time) { //capture problem numb
     var sol = vm.runInNewContext(data);
     if (sol == ans) {
       var elapsed_time = new Date().getTime() - start_time;
-      console.log(sol + " is the correct answer!")
-      console.log("Elapsed time was " + elapsed_time)
+      console.log(sol + " is the correct answer! Elapsed time was " + elapsed_time + ".")
 
       //add success to the results object
     }
     else {
-      console.log(sol + " is incorrect! The correct answer is " + ans)
+      console.log(sol + " is incorrect. The correct answer is " + ans + ".")
       // add failure to results object
     }
   });
